3881b535cf4eecbf46637b58d2dffb0e5531842c
Cole Kulig <candyman98@yahoo.com>
2017-10-17 21:47:36 -0500

    implemented Test and Rotor1 function
    Created Test and Rotor 1 basic function

--- src/enigmaMachine/Rotor1.java
+++ src/enigmaMachine/Rotor1.java
@@ -2,4 +2,20 @@ package enigmaMachine;
 
 public class Rotor1 {
 
+	private String[] inputValues = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+	private String[] rotor1Values = {"E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"};
+	private String convertedValue;
+	private int rotorPosition;
+	
+	
+	
+	public Rotor1(String letterInput) {
+		this.rotorPosition = inputValues[0].indexOf(letterInput.toString());
+		this.convertedValue = rotor1Values[rotorPosition];
+	}
+
+	public String getValue() {
+		return (convertedValue);
+	}
+	
 }
--- test/enigmaMachineTest/Rotor1Test.java
+++ test/enigmaMachineTest/Rotor1Test.java
@@ -1,5 +1,17 @@
 package enigmaMachineTest;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import enigmaMachine.Rotor1;
+
 public class Rotor1Test {
+
+	@Test 
+	public void validateRotorConversion() {
+		Rotor1 rotor = new Rotor1("C");
+		assertEquals(rotor.getValue(), "M");
+	}
 
 }
