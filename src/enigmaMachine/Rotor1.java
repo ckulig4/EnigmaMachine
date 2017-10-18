d1156d4739eb4c6ac217abe911a5148191c8d22c
Cole Kulig <candyman98@yahoo.com>
2017-10-17 22:03:33 -0500

    Fixed Rotor1 so that it passes test

--- src/enigmaMachine/Rotor1.java
+++ src/enigmaMachine/Rotor1.java
@@ -2,7 +2,7 @@ package enigmaMachine;
 
 public class Rotor1 {
 
-	private String[] inputValues = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+	private String inputValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	private String[] rotor1Values = {"E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"};
 	private String convertedValue;
 	private int rotorPosition;
@@ -10,7 +10,7 @@ public class Rotor1 {
 	
 	
 	public Rotor1(String letterInput) {
-		this.rotorPosition = inputValues[0].indexOf(letterInput);
+		this.rotorPosition = new String(inputValues).indexOf(letterInput);
 		this.convertedValue = rotor1Values[rotorPosition];
 	}
 
